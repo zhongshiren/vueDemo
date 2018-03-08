cf21ad1c3437effcafc52f581f28cc7d1aa6747b
Your Name <zhongshiren@git.com>
2018-03-08 23:01:15 +0800

    firstCommt

---
+++ src/components/regForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="login-form">
+       <div class="g-form">
+          <div class="g-form-line">
+             <div class="g-form-input">
+              <span class="g-form-label">用户名:</span>
+              <input type="text" palceholeder="请输入用户名">
+             </div>
+          </div>
+       <!--    <div class="g-form-line">
+             <div class="g-form-input">
+              <span class="g-form-label">密码:</span>
+              <input type="text" palceholeder="请输入密码">
+             </div>
+          </div> -->
+          <div  class="g-form-line">
+             <div class="g-form-btn">
+                 <a class="button">注册</a>
+             </div>
+          </div>
+
+       </div>       
+    </div>
+</template>
+<script>
+export default{
+data(){
+  return{}
+}
+
+}
+</script>
+
+<style scoped>
+
+</style> 
